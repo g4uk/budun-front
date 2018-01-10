e1a1aebd9d14038e3e8f6f5e00a786241765a40c
Roman Gnatiuk <rgnatiuk@attriboost.com>
2018-01-10 11:51:58 +0200

    add base configs

---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+// весь наш застосунок буде в папці app
+// вхідною точкою нашого застосунку буде файл app/index.js
+// бандлитись все буде в папку dist/bundle.js
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        'webpack-hot-middleware/client',
+        './app/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['babel'],
+            include: path.join(__dirname, 'app')
+        }]
+    }
+};
